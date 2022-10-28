4123dffd6d11b4373250d01ffd255c718194ef04
Pominov7 <Pominov7@mail.ru>
2022-10-28 15:08:10 +0700

    revision

--- src/main/java/org/top/springapiorder/db/OrderRepository.java
+++ src/main/java/org/top/springapiorder/db/OrderRepository.java
@@ -2,6 +2,6 @@ package org.top.springapiorder.db;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<OrderTEntity,Integer> {
+public interface OrderRepository extends CrudRepository<OrderTEntity, Integer> {
 
 }
--- src/main/java/org/top/springapiorder/service/OrderController.java
+++ src/main/java/org/top/springapiorder/service/OrderController.java
@@ -2,13 +2,9 @@ package org.top.springapiorder.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.top.springapiorder.db.OrderRepository;
 import org.top.springapiorder.db.OrderTEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
 
@@ -24,6 +20,7 @@ public class OrderController {
         return "pong";
     }
 
+    // CREATE
     @PostMapping(path = "/add")
     public @ResponseBody String addNewOrder(@RequestParam String name, @RequestParam String email
             , @RequestParam String phone, @RequestParam String address) {
@@ -36,13 +33,37 @@ public class OrderController {
         return "Saved";
     }
 
-    @GetMapping(path="/all")
+    // READ
+    @GetMapping(path = "/all")
     public @ResponseBody Iterable<OrderTEntity> getAllOrders() {
         return orderRepository.findAll();
     }
 
-    @PostMapping(path="/remove")
-    public @ResponseBody String removeUser(@RequestParam Integer id) {
+
+    // UPDATE
+    @PutMapping(path = "/update")
+    public @ResponseBody String updateOrder(@RequestBody OrderTEntity orderTEntity) {
+        Optional<OrderTEntity> orderDb = orderRepository.findById(orderTEntity.getIdF());
+        if (orderDb.isPresent()) {
+            OrderTEntity order = orderDb.get();
+            if (!orderTEntity.getNameF().isEmpty())
+                order.setNameF(orderTEntity.getNameF());
+            if (!orderTEntity.getEmailF().isEmpty())
+                order.setEmailF(orderTEntity.getEmailF());
+            if (!orderTEntity.getPhoneF().isEmpty())
+                order.setPhoneF(orderTEntity.getPhoneF());
+            if (!orderTEntity.getAddressF().isEmpty())
+                order.setAddressF(orderTEntity.getAddressF());
+
+            orderRepository.save(order);
+        }
+        return "Update";
+    }
+
+
+    // DELETE
+    @PostMapping(path = "/remove")
+    public @ResponseBody String removeOrder(@RequestParam Integer id) {
         Optional<OrderTEntity> deleted = orderRepository.findById(id);
         if (deleted.isPresent()) {
             orderRepository.delete(deleted.get());
